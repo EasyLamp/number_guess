7d8c6fb89aef5de1572a8e0118fd90171e52d947
Adam Rice <adamcoltonrice@gmail.com>
2016-09-23 06:11:52 -0600

    branch gh-pages

--- scripts.js
+++ scripts.js
@@ -2,12 +2,19 @@ var generatedNumber = numberGenerator();
 var submit = document.getElementById('playerButton');
 var output = document.getElementById('output');
 var input = document.getElementById('input');
-var clearInputButton = document.getElementById('clearInput')
-var grayResetButton = document.getElementById('resetButton')
+var clearInputButton = document.getElementById('clearInput');
+var grayResetButton = document.getElementById('resetButton');
+
+console.log(generatedNumber);
+
+submit.addEventListener('click', function (){
+  main();
+  clearFields();
+  grayResetButton.disabled = false;
+});
 
 function main() {
   var playerGuess = parseInt(input.value);
-
 
   if (isNaN(playerGuess)){
     return output.innerText = "You have not guessed a number.";
@@ -26,7 +33,8 @@ function main() {
   } else {
     output.innerText = "Shazam! Great work. Click Reset to play again.";
   }
-};
+}
+
 
 function clearFields() {
   input.value = "";
@@ -35,34 +43,24 @@ function clearFields() {
 
 input.addEventListener("keyup", function (){
 
-  if (input.value == ""){
+  if (input.value === ""){
      clearInputButton.disabled = true;
      grayResetButton.disabled = true;
   } else {
     clearInputButton.disabled = false;
     grayResetButton.disabled = false;
   }
-})
+});
 
 function resetG() {
-  //reset guess value
   input.value = "";
-  //set randomNumber to be a new random number
   generatedNumber = numberGenerator();
-  //update output text field
   output.innerText="";
-  //added to put function out of call stack
   clearInputButton.disabled = true;
   grayResetButton.disabled = true;
-  return false;
+  // return false;
 }
 
 function numberGenerator() {
   return Math.floor(Math.random() * 100) + 1;
 }
-
-submit.addEventListener('click', function (){
-  main();
-  clearFields();
-  grayResetButton.disabled = false;
-});
